621d795b71bb0cb2ea567cc2247789e979f37201
yvette <yvetteadan2000@gmail.com>
2019-11-20 16:16:09 +0300

    add news.py and news_test.py

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+    def __init__(self,source_name,title,description,poster, url):
+        self.source_name =source_name
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.url = url
+        
