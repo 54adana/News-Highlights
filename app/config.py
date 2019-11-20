4027dad8bd77f58143c793c0add187c29173f0e7
yvette <yvetteadan2000@gmail.com>
2019-11-20 16:06:53 +0300

    Add NEWS

--- app/config.py
+++ app/config.py
@@ -1,13 +1,12 @@
 class Config:
 
-    pass
+     
+    NEWS_API_BASE_URL ="https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}"
 
 class ProdConfig(Config):
 
     pass
 
 class DevConfig(Config):
-
-    pass
 
     DEBUG = True
